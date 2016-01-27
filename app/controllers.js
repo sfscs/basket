dde7b8e8ea64d462474d5187bff6408a32d6a0a2
Jake <jake@metart.com>
2016-01-26 22:30:28 -0800

    more wasting time via line breaks

--- app/controllers.js
+++ app/controllers.js
@@ -1,35 +1,40 @@
 angular.module('controllers', [])
-	.controller('UserCtrl', ['$scope', 'IdProvider', function ($scope, IdProvider) {
+	.controller('UserCtrl', ['$scope', 'IdProvider', 
+		function ($scope, IdProvider) {
 
 		// $scope.shoppingItems = (localStorage.getItem('shoppingItems')!==null) ? JSON.parse(localStorage.getItem('shoppingItems')) :	[buildShoppingItemObject('cheese'), buildShoppingItemObject('wallet'), buildShoppingItemObject('pants')];
 
-	}])
-	.controller('ListCtrl', ['$scope', 'Items', 'ItemCreator', function ($scope, Items, ItemCreator) {
-		$scope.shoppingItems = Items.data;
-		$scope.addShoppingItem = addShoppingItem;
-		$scope.removeShoppingItem = removeShoppingItem;
-
-		function addShoppingItem () {
-			if($scope.enteredShoppingItem) {
-				Items.add(ItemCreator(1, 1, $scope.enteredShoppingItem));
-				$scope.enteredShoppingItem = '';
-			}
-			else {
-				$scope.enteredShoppingItem = '';
-			}
-		};
-
-		function removeShoppingItem(shoppingItemID) {
-			Items.remove(shoppingItemID);
-		};
-
-	}])
-	.controller('AppCtrl', ['$scope', '$window', function($scope, $window) {
-		$scope.resetBasketApp = resetBasketApp;
-
-		function resetBasketApp() {
-			localStorage.clear();
-			$window.location.reload();
 		}
-	}])
+	])
+	.controller('ListCtrl', ['$scope', 'Items', 'ItemCreator', 
+		function ($scope, Items, ItemCreator) {
+			$scope.shoppingItems = Items.data;
+			$scope.addShoppingItem = addShoppingItem;
+			$scope.removeShoppingItem = removeShoppingItem;
+
+			function addShoppingItem () {
+				if($scope.enteredShoppingItem) {
+					Items.add(ItemCreator(1, 1, $scope.enteredShoppingItem));
+					$scope.enteredShoppingItem = '';
+				}
+				else {
+					$scope.enteredShoppingItem = '';
+				}
+			};
+
+			function removeShoppingItem(shoppingItemID) {
+				Items.remove(shoppingItemID);
+			};
+		}
+	])
+	.controller('AppCtrl', ['$scope', '$window', 'StorageService', 
+		function($scope, $window, StorageService) {
+			$scope.resetBasketApp = resetBasketApp;
+
+			function resetBasketApp() {
+				localStorage.clear();
+				$window.location.reload();
+			}
+		}
+	])
 ;
--- app/factories.js
+++ app/factories.js
@@ -6,76 +6,94 @@ angular.module('factories', [])
 		function get(key) {
 			return JSON.parse(localStorage.getItem(key));
 		}
+		function clearAll() {
+			localStorage.clear();
+		}
 		return {
 			get: get,
-			set: set
+			set: set,
+			clearAll: clearAll
 		};
 	})
-	.factory('IdService', ['StorageService', function IdServiceFactory(StorageService) {
-		var autoIncrement = (StorageService.get('autoIncrement') !== null) ? 
-			parseInt(StorageService.get('autoIncrement')) : 0;
-		StorageService.set('autoIncrement', autoIncrement);
-		return function() {
-			StorageService.set('autoIncrement', ++autoIncrement);
-			return autoIncrement - 1;
-		};
-	}])
-	.factory('UserCreator', ['IdService', function UserCreatorFactory(IdService) {
-		return function(userName) {
-			return {
-				"id": IdService(),
-				"name": userName.trim()
+	.factory('IdService', ['StorageService', 
+		function IdServiceFactory(StorageService) {
+			var autoIncrement = (StorageService.get('autoIncrement') !== null) ? 
+				parseInt(StorageService.get('autoIncrement')) : 0;
+			StorageService.set('autoIncrement', autoIncrement);
+			return function() {
+				StorageService.set('autoIncrement', ++autoIncrement);
+				return autoIncrement - 1;
 			};
-		};
-	}])
-	.factory('ListCreator', ['IdService', function ListCreatorFactory(IdService) {
-		return function(ownerID, listName) {
-			return {
-				"id": IdService(),
-				"name": listName.trim(),
-				"owner_id": ownerID,
-				"list_type": "normal",
-				"shared_with": [],
-				"added_date": Date.now()
-			}
-		};
-	}])
-	.factory('ItemCreator', ['IdService', function ItemCreatorFactory(IdService) {
-		return function(ownerID, listID, itemName) {
-			return {
-				"id": IdService(),
-				"name": itemName.trim(),
-				"checked": false,
-				"owner_id": ownerID,
-				"list_id": listID,
-				"assigned_to_id": ownerID,
-				"added_date": Date.now()
-			}
-		};
-	}])
-	.factory('Items', ['ItemCreator', 'StorageService', function ItemsFactory(ItemCreator, StorageService) {
-		items = {};
-		items.data = (StorageService.get('shoppingItems') !== null) ? 
-			StorageService.get('shoppingItems') :	
-			[ItemCreator(1,1,'cheese'), ItemCreator(1,1,'wallet'), ItemCreator(1,1,'pants')];
-		items.add = add;
-		items.remove = remove;
+		}
+	])
+	.factory('UserCreator', ['IdService', 
+		function UserCreatorFactory(IdService) {
+			return function(userName) {
+				return {
+					"id": IdService(),
+					"name": userName.trim()
+				};
+			};
+		}
+	])
+	.factory('ListCreator', ['IdService', 
+		function ListCreatorFactory(IdService) {
+			return function(ownerID, listName) {
+				return {
+					"id": IdService(),
+					"name": listName.trim(),
+					"owner_id": ownerID,
+					"list_type": "normal",
+					"shared_with": [],
+					"added_date": Date.now()
+				}
+			};
+		}
+	])
+	.factory('ItemCreator', ['IdService', 
+		function ItemCreatorFactory(IdService) {
+			return function(ownerID, listID, itemName) {
+				return {
+					"id": IdService(),
+					"name": itemName.trim(),
+					"checked": false,
+					"owner_id": ownerID,
+					"list_id": listID,
+					"assigned_to_id": ownerID,
+					"added_date": Date.now()
+				}
+			};
+		}
+	])
+	.factory('Items', ['ItemCreator', 'StorageService', 
+		function ItemsFactory(ItemCreator, StorageService) {
+			items = {};
+			
+			items.data = (StorageService.get('shoppingItems') !== null) ? 
+				StorageService.get('shoppingItems') :	
+				[ItemCreator(1,1,'cheese'), ItemCreator(1,1,'wallet'), ItemCreator(1,1,'pants')];
+			items.add = add;
+			items.remove = remove;
 
-		function add(item) {
-			items.data.push(item);
-			saveToStorage();
+			function add(item) {
+				items.data.push(item);
+				saveToStorage();
+			}
+
+			function remove(shoppingItemID) {
+				var i = items.data.filter(function(entry) {
+					return entry.id === shoppingItemID;
+				})[0];
+				var idx = items.data.indexOf(i);
+				items.data.splice(idx, 1);
+				saveToStorage();
+			}
+
+			function saveToStorage() {
+				StorageService.set('shoppingItems', items.data);
+			}
+
+			return items;
 		}
-		function remove(shoppingItemID) {
-			var i = items.data.filter(function(entry) {
-				return entry.id === shoppingItemID;
-			})[0];
-			var idx = items.data.indexOf(i);
-			items.data.splice(idx, 1);
-			saveToStorage();
-		}
-		function saveToStorage() {
-			StorageService.set('shoppingItems', items.data);
-		}
-		return items;
-	}])
+	])
 ;
